2a908f48ebc8b0a8ce304dbb4649867017e9ce74
Hugo Mayonobe <hugomayonobe@gmail.com>
2026-02-25 10:32:56 +0100

    Update API base URL to use the production server

--- lib/src/services/api_config.dart
+++ lib/src/services/api_config.dart
@@ -1,5 +1,5 @@
 const String apiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',
-  // defaultValue: 'https://kyno-backend.mayonobe.fr',
-  defaultValue: 'https://bea1-81-250-130-33.ngrok-free.app',
+  defaultValue: 'https://kyno-backend.mayonobe.fr',
+  // defaultValue: 'https://bea1-81-250-130-33.ngrok-free.app',
 );
